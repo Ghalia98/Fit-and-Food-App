0be5f5fa50f6645a1b9a1302b946ad8ce33c896b
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-02-15 17:54:26 +0200

    resolved conflict

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,10 @@ const User = require("../models/User")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-<<<<<<< HEAD
   res.render("index");
-=======
-
-    // render a view
-    res.render("index");
-
->>>>>>> 42b6e491ee6e4a88eca2d664d089a0e75f5746fa
 })
 
 function loginCheck() {
-<<<<<<< HEAD
   return (req, res, next) => {
     if (req.session.user) {
       // then the user making the request is logged in
@@ -24,23 +16,14 @@ function loginCheck() {
       next()
     } else {
       res.redirect('/login')
-=======
-    return (req, res, next) => {
-        if (req.session.user) {
-            // then the user making the request is logged in
-            // therefore user can proceed
-            next()
-        } else {
-            res.redirect('/login')
-        }
->>>>>>> 42b6e491ee6e4a88eca2d664d089a0e75f5746fa
     }
 
+  }
 }
 
 router.get('/profile', loginCheck(), (req, res, next) => {
-    const user = req.session.user
-    res.render('user/profile', { user })
+  const user = req.session.user
+  res.render('user/profile', { user })
 })
 
 module.exports = router;

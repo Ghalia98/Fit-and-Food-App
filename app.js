251882b2028632beb0173dbcffb6693b539788bd
Areeba Imran <areebaimran98@gmail.com>
2022-02-15 10:33:33 +0100

    Added recipe's detail page

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index");
 app.use("/", index);
 
+const recipes = require("./routes/recipes");
+app.use("/recipe", recipes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
